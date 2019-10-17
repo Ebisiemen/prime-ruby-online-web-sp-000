99861cbe65658be94c8473aa549213ca0d837106
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-17 13:20:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,2 @@
-# def prime?(number)
-#   if number == 2 || number % 2 == 0 
-#     true 
-#   else 
-#     false 
-#   end 
-# end   
-
-
 def prime?(number)
-  if number == (x < number ** y < number)
-    true
-  else 
-    false 
-  end   
-end     
+end   
